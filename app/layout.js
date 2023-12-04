9be9352056adef607bcbaef7f752bc50b3d16398
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2023-12-04 14:03:04 -0500

    Update layout.js file

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,15 @@
-import { Inter } from 'next/font/google'
+'use client'
+
 import './globals.css'
 import Header from '@/components/Header'
+import { useEffect } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'NextJS Course',
-  description: 'A course about NextJS',
-}
 
 export default function RootLayout({ children }) {
+  
   return (
     <html lang="en">
-      <body
-        className={inter.className}
-      >
+      <body>
         <Header />
         {children}
       </body>
